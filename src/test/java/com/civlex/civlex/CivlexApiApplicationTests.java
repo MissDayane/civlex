29d0eeae32feeda1d3f836d8b3330897f5f2dbe0
unknown <day_pontess@hotmail.com>
2025-10-26 20:29:45 -0300

    Primeiro Commit

---
+++ src/test/java/com/civlex/civlex/CivlexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civlex.civlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivlexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
